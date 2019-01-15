935ad0985749fb4362cc4cce7262acb8f2e48328
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2019-01-16 01:59:15 +0200

    reformat and add additional options

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 # This is a helper script for discovering the modules inside the package and
 # running the doctests.
-# usage: python3 tests.py [-r] -- option -r reports the modules found
+# usage: python3 tests.py [-r] [-a] [-e=filename]
+#   -- option -r reports the modules found
+#   -- option -a to include all python files, including those in subpackages
+#   -- option -e tp exclude a file
 # any other doctest module options can be passed such as -f and -v
 # any modules outside the package folder are not considered for testing.
 
@@ -11,21 +14,41 @@ from pathlib import Path
 
 if __name__ == '__main__':
     # discover all python files inside the package and run the doc tests
-    report = any([arg == '-r' for arg in sys.argv])
+    # refactor to use argparse if got big
+    excluded, report, search_in_all = [], False, False
+    for arg in sys.argv:
+        if arg.startswith('-e'):
+            excluded.append(arg.split('=', maxsplit=1))
+        elif arg == '-r':
+            report = True
+        elif arg == '-a':
+            search_in_all = True
+
     modules = []
     package = Path('.') / 'empyro'
+
     print('DISCOVERING MODULES...')
-    for module_path in package.glob('**/*.py'):
+
+    glob_pattern = '**/*.py' if search_in_all else '*.py'
+
+    for module_path in package.glob(glob_pattern):
+        print(module_path)
+        if module_path.stem in excluded:
+            continue
         module_parent = '.'.join(module_path.parts[:-1])
         module_name = module_parent + '.' + module_path.stem
         modules.append(importlib.import_module(module_name))
+
     if report:
         print('\n== {} MODULES FOUND =='
               .format(len(modules)))
         for module in modules:
-            print('  -- {}'.format(module.__name__))
+            print('  - {}'.format(module.__name__))
         print('\n')
+
     print('TESTING MODULES...')
+
     for module in modules:
         doctest.testmod(module, exclude_empty=True)
+
     print('DONE')
