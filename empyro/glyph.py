b411dd136768768328a6918a1764094449a8119b
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2019-01-20 22:46:24 +0200

    refactor Glyph to be a named tuple

--- empyro/glyph.py
+++ empyro/glyph.py
@@ -3,28 +3,35 @@
 defines Glyph class.
 """
 
-from typing import Union, Text
+from typing import NamedTuple, Union, Text
 
 from . import color
 from .color import Color
 from .charcode import CharCode
 
 
-class Glyph:
+_Glyph = NamedTuple('Glyph', [
+    ('code', Union[Text, CharCode]),
+    ('fg_color', Color),
+    ('bg_color', Color),
+])
+
+
+class Glyph(_Glyph):
     """Represent a glyph
 
     code     -- a unicode code point or a character (str of length 1).
     fg_color -- the foreground color of the glyph.
     bg_color -- the background color of the glyph.
     """
-    def __init__(self, code: Union[Text, CharCode],
+    def __new__(cls, code: Union[Text, CharCode],
                  fg_color: Color = None,
                  bg_color: Color = None):
         if not isinstance(code, CharCode):
-                code = CharCode(ord(code))
-        self.code = code
-        self.fg_color = color.WHITE if fg_color is None else Color(*fg_color)
-        self.bg_color = color.BLACK if bg_color is None else Color(*bg_color)
+            code = CharCode(ord(code))
+        fg_color = color.WHITE if fg_color is None else Color(*fg_color)
+        bg_color = color.BLACK if bg_color is None else Color(*bg_color)
+        return super().__new__(cls, code, fg_color, bg_color)
 
 
 CLEAR = Glyph(CharCode.SPACE)
